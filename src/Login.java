96ea28b794feb447fb9393cd2cfd9758752937e9
ASUS <pnn_spk@hotmail.com>
2018-09-03 21:04:10 +0700

    First Commit to Git

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,22 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
     private JTextField txtUserLogin;
     private JPasswordField pwdUserPassword;
     private JButton btnSubmit;
     private JButton btnClear;
+    private JPanel mainPanel;
+
+    public Login() {
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,txtUserLogin.getText()
+                        + " " + new String(pwdUserPassword.getPassword()));
+            }
+        });
+    }
+
 }
