fa7c33b91c4feb2b09b77a3c39e9b26303a2e0eb
ASUS <pnn_spk@hotmail.com>
2018-09-03 20:52:15 +0700

    First Commit to Git

---
+++ src/Login.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Login {
+    private JTextField txtUserLogin;
+    private JPasswordField pwdUserPassword;
+    private JButton btnSubmit;
+    private JButton btnClear;
+}
